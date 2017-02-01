9c8564dceee09caea80a8385cd41e9037a4ddbd4
Stefan Gehn <stefan@froglogic.com>
2017-02-01 14:45:28 +0100

    Add Windows build script
    Add a script to build treefrog-framework in a configuration suitable
    for a redistributable teamserver.

---
+++ frogbuild.bat
@@ -0,0 +1,17 @@
+echo on
+setlocal
+IF [%1] == [] (
+        echo Missing destination directory argument
+        goto :EOF
+        )
+call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86 || goto :EOF
+echo on
+set SRC=%~dp0
+set DST=%1
+set PATH=Q:\binary\MSVC14\x86\5.6.0\5.6\msvc2015\bin;%PATH%
+call configure.bat --prefix "%DST%" || goto :EOF
+echo on
+cd %SRC%\src
+nmake install || goto :EOF
+cd %SRC%\tools
+nmake install || goto :EOF
